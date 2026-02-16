c87ba45cf67df78455121e2ee223aff584c34764
keiseiisobe <keisei.isobe@gmail.com>
2026-02-16 09:39:41 +0900

    added py

--- slides/abstract_machine/halting_problem.py
+++ slides/abstract_machine/halting_problem.py
@@ -1,6 +1,24 @@
+# P = program
+# I = input
+# --> P(I)
+
+# H(P, I)
+# --> P(I)が止まるか止まらないか
+# --> このようなHが存在するか
+
+def H(P, I):
+    P(I)
+    return "YES"
+
+# P(P)が止まる時はQ(P)は止まらない
+# P(P)が止まらない時はQ(P)は止まる
 def Q(P):
-    answer = H(P, P)
+    answer = H(P, P) # P(P)
+
     if answer == "YES":
-        LoopForever()
+        Loop_Forever()
     else:
         Halt()
+
+Q(Q)
+        
